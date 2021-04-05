4954eeafcbb57621a35f9cbe4708cc9c2fca5845
Christian Lascsak <christian.lascsak7@gmail.com>
2021-04-05 23:06:09 +0200

    rename birdEntity to playerEntity

--- VulkanEngine/game/EventListeners/PlayerController.h
+++ VulkanEngine/game/EventListeners/PlayerController.h
@@ -47,20 +47,20 @@ namespace game
         if (event.idata3 == GLFW_RELEASE)
             return false;
 
-        auto birdEntity = getSceneManagerPointer()->getSceneNode(EntityName::Player);
+        auto playerEntity = getSceneManagerPointer()->getSceneNode(EntityName::Player);
         switch (event.idata1)
         {
         case GLFW_KEY_A:
-            translate = birdEntity->getTransform() * glm::vec4(-1.0, 0.0, 0.0, 1.0); //left
+            translate = playerEntity->getTransform() * glm::vec4(-1.0, 0.0, 0.0, 1.0); //left
             break;
         case GLFW_KEY_D:
-            translate = birdEntity->getTransform() * glm::vec4(1.0, 0.0, 0.0, 1.0); //right
+            translate = playerEntity->getTransform() * glm::vec4(1.0, 0.0, 0.0, 1.0); //right
             break;
         case GLFW_KEY_W:
-            translate = birdEntity->getTransform() * glm::vec4(0.0, 0.0, 1.0, 1.0); //forward
+            translate = playerEntity->getTransform() * glm::vec4(0.0, 0.0, 1.0, 1.0); //forward
             break;
         case GLFW_KEY_S:
-            translate = birdEntity->getTransform() * glm::vec4(0.0, 0.0, -1.0, 1.0); //back
+            translate = playerEntity->getTransform() * glm::vec4(0.0, 0.0, -1.0, 1.0); //back
             break;
         case GLFW_KEY_Q:
             translate = glm::vec4(0.0, -1.0, 0.0, 1.0); //down
@@ -76,13 +76,13 @@ namespace game
             angle = (float)event.dt * 1.0f;
             rot4 = glm::vec4(0.0, 1.0, 0.0, 1.0);
             break;
-        case GLFW_KEY_UP:                                                      //pitch rotation is in cam/local space
-            angle = (float)event.dt * 1.0f;                                    //pitch angle
-            rot4 = birdEntity->getTransform() * glm::vec4(1.0, 0.0, 0.0, 1.0); //x axis from local to parent space!
+        case GLFW_KEY_UP:                                                        //pitch rotation is in cam/local space
+            angle = (float)event.dt * 1.0f;                                      //pitch angle
+            rot4 = playerEntity->getTransform() * glm::vec4(1.0, 0.0, 0.0, 1.0); //x axis from local to parent space!
             break;
-        case GLFW_KEY_DOWN:                                                    //pitch rotation is in cam/local space
-            angle = (float)event.dt * -1.0f;                                   //pitch angle
-            rot4 = birdEntity->getTransform() * glm::vec4(1.0, 0.0, 0.0, 1.0); //x axis from local to parent space!
+        case GLFW_KEY_DOWN:                                                      //pitch rotation is in cam/local space
+            angle = (float)event.dt * -1.0f;                                     //pitch angle
+            rot4 = playerEntity->getTransform() * glm::vec4(1.0, 0.0, 0.0, 1.0); //x axis from local to parent space!
             break;
 
         default:
@@ -91,12 +91,12 @@ namespace game
 
         ///add the new translation vector to the previous one
         glm::vec3 trans = m_movement_speed * glm::vec3(translate.x, translate.y, translate.z);
-        birdEntity->multiplyTransform(glm::translate(glm::mat4(1.0f), (float)event.dt * trans));
+        playerEntity->multiplyTransform(glm::translate(glm::mat4(1.0f), (float)event.dt * trans));
 
         ///combination of yaw and pitch, both wrt to parent space
         glm::vec3 rot3 = glm::vec3(rot4.x, rot4.y, rot4.z);
         glm::mat4 rotate = glm::rotate(glm::mat4(1.0), angle, rot3);
-        birdEntity->multiplyTransform(rotate);
+        playerEntity->multiplyTransform(rotate);
 
         return true;
     }
