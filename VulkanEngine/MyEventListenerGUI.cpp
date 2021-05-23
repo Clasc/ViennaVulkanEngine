89db7a84f993f3266f8fd2840846cd9d1d85eb29
Christian Lascsak <christian.lascsak@anecon.com>
2021-05-23 18:03:49 +0200

    gui-win (c) refactor rows

--- VulkanEngine/MyEventListenerGUI.cpp
+++ VulkanEngine/MyEventListenerGUI.cpp
@@ -71,45 +71,44 @@ namespace ve {
 	void MyEventListenerGUI::renderNodeSubMenu(nk_context* ctx, VESceneNode* node) {
 		char outbuffer[200];
 		nk_layout_row_dynamic(ctx, 45, 6);
-		{
-			auto pos = node->getPosition();
-			char xbuf[20];
-			char ybuf[20];
-			char zbuf[20];
 
-			auto xLabel = "x: " + std::to_string(pos.x);
-			auto yLabel = "y: " + std::to_string(pos.y);
-			auto zLabel = "z: " + std::to_string(pos.z);
+		auto pos = node->getPosition();
+		char xbuf[20];
+		char ybuf[20];
+		char zbuf[20];
 
-			sprintf(outbuffer, xLabel.c_str());
-			nk_label(ctx, outbuffer, NK_TEXT_LEFT);
-			nk_edit_string_zero_terminated(ctx, NK_EDIT_FIELD, xbuf, sizeof(xbuf) - 1, nk_filter_decimal);
+		auto xLabel = "x: " + std::to_string(pos.x);
+		auto yLabel = "y: " + std::to_string(pos.y);
+		auto zLabel = "z: " + std::to_string(pos.z);
 
-			sprintf(outbuffer, yLabel.c_str());
-			nk_label(ctx, outbuffer, NK_TEXT_LEFT);
-			nk_edit_string_zero_terminated(ctx, NK_EDIT_FIELD, ybuf, sizeof(ybuf) - 1, nk_filter_decimal);
+		sprintf(outbuffer, xLabel.c_str());
+		nk_label(ctx, outbuffer, NK_TEXT_LEFT);
+		nk_edit_string_zero_terminated(ctx, NK_EDIT_FIELD, xbuf, sizeof(xbuf) - 1, nk_filter_decimal);
 
-			sprintf(outbuffer, zLabel.c_str());
-			nk_label(ctx, outbuffer, NK_TEXT_LEFT);
-			nk_edit_string_zero_terminated(ctx, NK_EDIT_FIELD, zbuf, sizeof(zbuf) - 1, nk_filter_decimal);
-		}
+		sprintf(outbuffer, yLabel.c_str());
+		nk_label(ctx, outbuffer, NK_TEXT_LEFT);
+		nk_edit_string_zero_terminated(ctx, NK_EDIT_FIELD, ybuf, sizeof(ybuf) - 1, nk_filter_decimal);
+
+		sprintf(outbuffer, zLabel.c_str());
+		nk_label(ctx, outbuffer, NK_TEXT_LEFT);
+		nk_edit_string_zero_terminated(ctx, NK_EDIT_FIELD, zbuf, sizeof(zbuf) - 1, nk_filter_decimal);
+		
 	}
 	
 	void MyEventListenerGUI::renderLightSubMenu(nk_context* ctx, VELight* light) {
 		char outbuffer[200];
 		nk_layout_row_dynamic(ctx, 45, 1);
-		{
-			auto originalCol = light->m_col_ambient;
-			nk_color lightColor = { originalCol.r * 255,originalCol.g * 255, originalCol.b * 255, originalCol.a * 255, };
-			nk_label(ctx, outbuffer, NK_TEXT_LEFT);
-			nk_label(ctx, "Ambient Light Color", NK_TEXT_LEFT);
-			nk_layout_row_dynamic(ctx, 250, 1);
-			nk_color_pick(ctx, &lightColor, NK_RGBA);
+		
+		auto originalCol = light->m_col_ambient;
+		nk_color lightColor = { originalCol.r * 255,originalCol.g * 255, originalCol.b * 255, originalCol.a * 255, };
+		nk_label(ctx, outbuffer, NK_TEXT_LEFT);
+		nk_label(ctx, "Ambient Light Color", NK_TEXT_LEFT);
+		nk_layout_row_dynamic(ctx, 250, 1);
+		nk_color_pick(ctx, &lightColor, NK_RGBA);
 
-			light->m_col_ambient = { float(lightColor.r) / 255, float(lightColor.g) / 255, float(lightColor.b) / 255, float(lightColor.a) / 255 };
-			//light->m_col_diffuse = { float(lightColor.r) / 255, float(lightColor.g) / 255, float(lightColor.b) / 255, float(lightColor.a) / 255 };
-			//light->m_col_specular = { float(lightColor.r) / 255, float(lightColor.g) / 255, float(lightColor.b) / 255, float(lightColor.a) / 255 };	
-		}
+		light->m_col_ambient = { float(lightColor.r) / 255, float(lightColor.g) / 255, float(lightColor.b) / 255, float(lightColor.a) / 255 };
+		//light->m_col_diffuse = { float(lightColor.r) / 255, float(lightColor.g) / 255, float(lightColor.b) / 255, float(lightColor.a) / 255 };
+		//light->m_col_specular = { float(lightColor.r) / 255, float(lightColor.g) / 255, float(lightColor.b) / 255, float(lightColor.a) / 255 };	
 	}
 
 	bool MyEventListenerGUI::isLight(VESceneNode* node) {
